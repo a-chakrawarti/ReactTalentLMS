4a4c87554b61ddeccc74837a9f4524fd4cae2f50
CURVX <chakrawarti.anupam@gmail.com>
2021-04-14 00:48:03 +0530

    Final draft assignment-1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,60 @@
+import React, { useState } from "react";
 import "./App.css";
-import Counter from "./components/counter";
+import Counters from "./components/counters";
+import NavBar from "./components/navbar";
 
 function App() {
+  const initValue = [
+    { id: 1, value: 5 },
+    { id: 2, value: 4 },
+    { id: 3, value: 3 },
+    { id: 4, value: 0 },
+    { id: 5, value: 2 },
+  ];
+  const [counters, setCounter] = useState(initValue);
+
+  const handleIncrement = (data) => {
+    const updatedCounters = [...counters];
+    const index = updatedCounters.indexOf(data);
+    updatedCounters[index] = { ...data };
+    updatedCounters[index].value++;
+    setCounter(updatedCounters);
+  };
+
+  const handleDecrement = (data) => {
+    const updatedCounters = [...counters];
+    const index = updatedCounters.indexOf(data);
+    updatedCounters[index] = { ...data };
+    updatedCounters[index].value--;
+    setCounter(updatedCounters);
+  };
+
+  const handleDelete = (counterId) => {
+    const updatedCounters = counters.filter((c) => c.id !== counterId);
+    setCounter(updatedCounters);
+  };
+
+  const handleReset = () => {
+    const updatedCounters = counters.map((counter) => {
+      counter.value = 0;
+      return counter;
+    });
+    setCounter(updatedCounters);
+  };
   return (
     <>
-      <Counter />
+      <NavBar
+        totalCounters={counters.filter((counter) => counter.value > 0).length}
+      />
+      <main className="container">
+        <Counters
+          counters={counters}
+          onIncrement={handleIncrement}
+          onDecrement={handleDecrement}
+          onDelete={handleDelete}
+          onReset={handleReset}
+        />
+      </main>
     </>
   );
 }
--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,17 +1,36 @@
-import React,  { useState } from 'react';
+import React from "react";
 
-const Counter = () => {
-    const [count, setCount] = useState(0);
-    return (<>
-    <span className={getBadgeClasses(count)}>{count ? count : 'Zero'}</span>
-    <button className="btn btn-secondary btn-sm">+</button>
-    </>);
-}
+const Counter = ({ data, onIncrement, onDecrement, onDelete }) => {
+  return (
+    <div>
+      <span
+        style={{ fontSize: "14px" }}
+        className={getBadgeClasses(data.value)}>
+        {data.value ? data.value : "Zero"}
+      </span>
+      <button
+        onClick={() => onIncrement(data)}
+        className="btn btn-secondary btn-sm m-2">
+        Increment
+      </button>
+      <button
+        onClick={() => onDecrement(data)}
+        className="btn btn-secondary btn-sm m-2">
+        Decrement
+      </button>
+      <button
+        onClick={() => onDelete(data.id)}
+        className="btn btn-danger btn-sm m-2">
+        Delete
+      </button>
+    </div>
+  );
+};
 
 function getBadgeClasses(count) {
-    let classes = "badge m-2 badge-";
-    classes += count ? "primary" : "warning";
-    return classes;
+  let classes = "badge m-2 badge-";
+  classes += count ? "primary" : "warning";
+  return classes;
 }
- 
+
 export default Counter;
