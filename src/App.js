6253999fe8418c93607505b345a34245ee6e5973
CURVX <chakrawarti.anupam@gmail.com>
2021-04-19 16:30:52 +0530

    Completed Project#2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,59 @@
+import React, { useEffect, useState } from "react";
+import Tours from "./components/Tours";
+import Loading from "./components/Loading";
 import "./App.css";
 
 function App() {
-  return <div>AppJS</div>;
+  const url = "https://course-api.com/react-tours-project";
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const res = await fetch(url);
+      const tours = await res.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+
+    // console.log(tours);
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+  if (loading) {
+    <main>
+      <Loading />
+    </main>;
+  }
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>No destination found!</h2>
+          <button onClick={fetchTours} className="btn">
+            Refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  );
 }
 
 export default App;
---
+++ src/components/Tours.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Tour from "./Tour";
+
+const Tours = ({ tours, removeTour }) => {
+  return (
+    <>
+      <div className="title">
+        <h2>Tourist destination</h2>
+        <div className="underline"></div>
+      </div>
+      <div>
+        {tours.map((tour) => {
+          return <Tour key={tour.id} {...tour} removeTour={removeTour} />;
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Tours;
